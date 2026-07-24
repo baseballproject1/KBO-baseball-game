f4ee670f027f7ec27d9ca94608558901e9b7b4c3
baseballproject1 <ryuhj0829@gmail.com>
2026-07-24 13:35:09 +0900

    Create teams.js

---
+++ teams.js
@@ -0,0 +1,91 @@
+// teams.js
+
+const TEAMS = {
+
+    LG: {
+        id: "LG",
+        name: "LG 트윈스",
+        players: LG_PLAYERS
+    },
+
+    SSG: {
+        id: "SSG",
+        name: "SSG 랜더스",
+        players: SSG_PLAYERS
+    },
+
+    KIA: {
+        id: "KIA",
+        name: "KIA 타이거즈",
+        players: KIA_PLAYERS
+    },
+
+    SAMSUNG: {
+        id: "SAMSUNG",
+        name: "삼성 라이온즈",
+        players: SAMSUNG_PLAYERS
+    },
+
+    KT: {
+        id: "KT",
+        name: "KT 위즈",
+        players: KT_PLAYERS
+    },
+
+    DOOSAN: {
+        id: "DOOSAN",
+        name: "두산 베어스",
+        players: DOOSAN_PLAYERS
+    },
+
+    LOTTE: {
+        id: "LOTTE",
+        name: "롯데 자이언츠",
+        players: LOTTE_PLAYERS
+    },
+
+    HANWHA: {
+        id: "HANWHA",
+        name: "한화 이글스",
+        players: HANWHA_PLAYERS
+    },
+
+    KIWOOM: {
+        id: "KIWOOM",
+        name: "키움 히어로즈",
+        players: KIWOOM_PLAYERS
+    },
+
+    NC: {
+        id: "NC",
+        name: "NC 다이노스",
+        players: NC_PLAYERS
+    }
+
+};
+
+
+// 팀 정보 가져오기
+function getTeam(teamId){
+
+    return TEAMS[teamId];
+
+}
+
+
+// 팀 선수 가져오기
+function getTeamPlayers(teamId){
+
+    return TEAMS[teamId].players;
+
+}
+
+
+// 선수 찾기
+function getPlayer(teamId, playerId){
+
+    return TEAMS[teamId].players.find(
+        player => player.id === playerId
+    );
+
+}
